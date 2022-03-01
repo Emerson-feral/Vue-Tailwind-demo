2224b2dfd66f4370b06f4f923db9338e4bd1a98c
Emerson Fernandes <emerson_feral@hotmail.com>
2022-03-01 20:29:09 +0100

    Nesting components

--- clicker/src/App.vue
+++ clicker/src/App.vue
@@ -1,14 +1,24 @@
 <template>
-  <HelloWorld/>
+  <div class="flex">
+    <NavBar/>
+    <Clicker/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Clicker from './components/Clicker.vue'
+import NavBar from './components/NavBar.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  export default{
+    name:"App",
+    components: { 
+      Clicker,
+      NavBar 
+    },
   }
-}
 </script>
+
+
+<style>
+
+</style>
